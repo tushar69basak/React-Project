c4da9858c4da01866bc92f12e452d72139089b27
tushar69basak <tusharbasak2017@gmail.com>
2024-02-09 19:28:24 +0530

    create final version of calculator

--- 3-calculator/src/components/ButtonsContainer.jsx
+++ 3-calculator/src/components/ButtonsContainer.jsx
@@ -1,6 +1,6 @@
 import styles from "./ButtonsContainer.module.css";
 
-const ButtonsContainer = () => {
+const ButtonsContainer = ({ onButtonClick }) => {
   const buttonNames = [
     "C",
     "1",
@@ -24,10 +24,13 @@ const ButtonsContainer = () => {
   return (
     <div className={styles.buttonsContainer}>
       {buttonNames.map((buttonName) => (
-        <button className={styles.button}>{buttonName}</button>
+        <button
+          className={styles.button}
+          onClick={() => onButtonClick(buttonName)}
+        >
+          {buttonName}
+        </button>
       ))}
     </div>
   );
 };
-
-export default ButtonsContainer;
